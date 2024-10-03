336a0ddc3b788661891421a2e152f0b0735659be
Sankar491 <sivasankarrcks@gmail.com>
2024-10-03 12:12:46 +0530

    added private and database peering

--- peering.tf
+++ peering.tf
@@ -19,12 +19,27 @@ resource "aws_route" "public_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0  
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0  
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
+}
+
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = data.aws_vpc.default.main_route_table_id
   # destination_cidr_block    = var.public_subnet_cidrs[0]
   # destination_cidr_block    = var.public_subnet_cidrs[1]
   # destination_cidr_block    = var.public_subnet_cidrs[*]
-  destination_cidr_block    = var.public_subnet_cidrs[count.index]
+  # destination_cidr_block    = var.public_subnet_cidrs[count.index]
+  destination_cidr_block = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
